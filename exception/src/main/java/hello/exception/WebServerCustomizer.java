591bda039d8adfb1a5c510fcd74b0ef9ad2a23a4
eugene0318 <nttott@gmail.com>
2023-09-25 02:08:42 +0900

    ~exception filter

---
+++ exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,25 @@
+package hello.exception;
+
+import org.apache.tomcat.util.descriptor.web.ErrorPage;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+	@Override
+	public void customize(ConfigurableWebServerFactory factory) {
+		org.springframework.boot.web.server.ErrorPage errorPage404 = new org.springframework.boot.web.server.ErrorPage(
+				HttpStatus.NOT_FOUND, "/error-page/400");
+		org.springframework.boot.web.server.ErrorPage errorPage500 = new org.springframework.boot.web.server.ErrorPage(
+				HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+		org.springframework.boot.web.server.ErrorPage errorPageEx = new org.springframework.boot.web.server.ErrorPage(
+				RuntimeException.class, "/error-page/500");
+
+		factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+
+	}
+
+}
