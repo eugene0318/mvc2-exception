59cbd4433ba73397c0c077a5f8be907082614117
eugene0318 <nttott@gmail.com>
2023-09-30 01:16:47 +0900

    ~ResponseStatusExceptionResolver

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,9 +1,12 @@
 package hello.exception.api;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import hello.exception.exception.BadRequestException;
 import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,12 +25,23 @@ public class ApiExceptionController {
 		if (id.equals("bad")) {
 			throw new IllegalArgumentException("잘못된 입력 값");
 		}
-		
-		if(id.equals("user-ex")) {
+
+		if (id.equals("user-ex")) {
 			throw new UserException("사용자 오류");
 		}
 
 		return new MemberDto(id, "hello" + id);
+	}
+
+	@GetMapping("/api/response-status-ex1")
+	public String responseStatusEx1() {
+		throw new BadRequestException();
+	}
+
+	@GetMapping("/api/response-status-ex2")
+	public String responseStatusEx2() {
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error-bad", new IllegalArgumentException());
+
 	}
 
 	@Data
